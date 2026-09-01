9bf78b952c2fa9cd9993d38713aecc1e8f27fe13
DaywisonBruno <brunodaywison4@gmail.com>
2026-09-01 11:55:05 -0300

    Criação de curser.html para entrada do portfolio
    - Criação de  html
    - Adição de vídeo no cursos
    - Troca de capa de experiência e formação.
    - Ajuste pendente na .secao1 e secao3

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ document.querySelector(".open").addEventListener("click", function() {
     
 })
 
+const video = document.getElementById('video-intro');
+const boasVindas = document.getElementById('boas-vindas');
+
+video.addEventListener('ended', function() {
+  document.querySelector('.tela-video').style.display = 'none';
+  boasVindas.classList.add('visivel');
+});
+
